2ce3e392982a94a6c61b3b19f7c357e5576b6403
Clasher370 <tkasha89@gmail.com>
2017-10-17 22:59:50 +0300

    factories

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,21 +1,33 @@
 require 'rails_helper'
 
 describe 'Posts' do
+  let!(:user) { create(:user) }
+  let!(:another_user) { create(:user) }
+
+  let(:ip) { Faker::Internet.ip_v4_address }
+  let(:another_ip) { Faker::Internet.ip_v4_address }
+
+  let!(:post1) { create(:post, user_id: user.id, ip: ip) }
+  let!(:post2) { create(:post, user_id: user.id, ip: another_ip) }
+  let!(:post3) { create(:post, user_id: another_user.id, ip: another_ip) }
+
+  let!(:rating1) { Rating.create(post_id: post1.id, post_rating: 4) }
+  let!(:rating2) { Rating.create(post_id: post2.id, post_rating: 5) }
+
   context '#top' do
-    let!(:user) { User.create(login: 'Vasya') }
+    before { post '/top_post', params: { limit: 2 } }
 
-    let!(:post1) { Post.create(title: 'Post1', user_id: user.id)}
-    let!(:post2) { Post.create(title: 'Post2', user_id: user.id)}
-
-    let!(:rating1) { Rating.create(post_id: post1.id, post_rating: 4) }
-    let!(:rating2) { Rating.create(post_id: post2.id, post_rating: 5) }
-
+    it { expect(response).to have_http_status 200 }
 
     it 'is return top rated posts' do
-      post '/top_post', params: { limit: 2 }
+      expect(JSON.parse(response.body)[0]['title']).to eq post2.title
+    end
+  end
 
-      expect(response).to have_http_status 200
-      expect(JSON.parse(response.body)[0]['title']).to eq 'Post2'
+  context '#ip_with_more_when_one_users' do
+    it 'is return post if more 1 user' do
+      get '/ips'
+      expect(JSON.parse(response.body)[0]['ip']).to eq another_ip
     end
   end
 end
