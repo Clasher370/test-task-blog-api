f5b9c3c0336742620ad322e9f10ba47437e01cea
Clasher370 <tkasha89@gmail.com>
2017-11-09 11:17:55 +0300

    fix

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -7,10 +7,13 @@ describe 'Users' do
     let!(:post) { create :post, ip: ip }
     let!(:another_post) { create :post, ip: ip }
 
+    before { get '/users/by_ip' }
+
+    it { expect(response).to have_http_status 200 }
+
     it 'return array of login by ip if more when one user' do
-      get '/users/by_ip'
-      expect(response).to have_http_status 200
-      expect(JSON.parse(response.body)).to eq([{ "#{ip}" => [post.user.login, another_post.user.login] }])
+      expect(JSON.parse(response.body))
+          .to eq([{ "#{ip}" => [post.user.login, another_post.user.login] }])
     end
   end
 end
